e2049fce13f8cc78583a90b7fc6330ed9c2ca734
Damian Lavin <damianlavin75@gmail.com>
2026-04-02 09:33:08 -0700

    Update: Gutted excess comments, extra functions, and helps. Great Save Point.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,10 @@
 import "./App.css";
 
 import { useRef } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { ScrollControls } from "@react-three/drei";
 
 import Experience from "./Experience";
-
-// function CameraManager() {
-//   const { camera } = useThree();
-
-//   // Create Leva controls
-//   const { cameraPosition } = useControls("Camera", {
-//     cameraPosition: {
-//       value: [2, 0.2, 0],
-//       step: 0.1,
-//       onChange: (v) => {
-//         camera.position.set(v[0], v[1], v[2]);
-//       },
-//     },
-//     fov: 75,
-//   });
-
-//   return null; // Component only acts as a manager
-// }
 
 function App() {
   const { cameraRef } = useRef();
@@ -35,10 +17,8 @@ function App() {
       camera={{
         fov: 15,
         position: [5, 2, 15],
-        // position: [0, 0, 0], NO
       }}
     >
-      {/* <CameraManager /> */}
       <ScrollControls pages={1} damping={0.1}>
         <Experience />
       </ScrollControls>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,43 +1,17 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import { useLayoutEffect, useRef } from "react";
-import { OrbitControls, ScrollControls, useScroll } from "@react-three/drei";
+import { useRef } from "react";
+import { useScroll } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 
-import { useControls } from "leva";
-
 import { Skyscraper } from "./Skyscraper";
-
-export const FLOOR_HEIGHT = 0.5;
-// export const FLOORS = 3;
-
-gsap.registerPlugin(ScrollTrigger);
 
 function Experience() {
   // This grabs the <Canvas> camera
   const { camera } = useThree();
   const scroll = useScroll();
   const timeline = useRef();
-
-  const groupRef = useRef();
-  const floorRef = useRef();
-
-  // HELPERS
-  // const floorRotation = useControls("Rotation Floor", {
-  //   x: { value: 0, min: 0, max: 10, step: 0.001 },
-  //   y: { value: 1.56, min: 0, max: 10, step: 0.01 },
-  //   z: { value: 0, min: 0, max: 10, step: 0.01 },
-  // });
-  // const floorPosition = useControls("Floor Position", {
-  //   x: { value: 0.32, min: 0, max: 10, step: 0.001 },
-  //   y: { value: 0.1, min: 0, max: 10, step: 0.01 },
-  //   z: { value: 0, min: 0, max: 10, step: 0.01 },
-  // });
-  // const bckgndColor = useControls("Background Color", {
-  //   color: { value: "grey" },
-  // });
 
   // 1. Create a "Master Timeline" that is PAUSED
   useGSAP(() => {
@@ -65,13 +39,11 @@ function Experience() {
     <>
       <color attach="background" args={["grey"]} />
       <ambientLight intensity={1.5} />
-      <group ref={groupRef}>
-        <Skyscraper />
-        <mesh ref={floorRef} position={[0.32, 0.1, 0]} rotation={[0, 1.56, 0]}>
-          <boxGeometry args={[5, 0, 5]} />
-          <meshStandardMaterial color="green" />
-        </mesh>
-      </group>
+      <Skyscraper />
+      <mesh position={[0.32, 0.1, 0]} rotation={[0, 1.56, 0]}>
+        <boxGeometry args={[5, 0, 5]} />
+        <meshStandardMaterial color="green" />
+      </mesh>
       <axesHelper />
       <gridHelper />
     </>

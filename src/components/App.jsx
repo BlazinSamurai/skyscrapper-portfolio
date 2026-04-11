76247f30cb88ccd9d34fe799a16904af149b10a7
Damian Lavin <damianlavin75@gmail.com>
2026-04-11 14:44:58 -0700

    Added: 'Fog' layer, hided vertical scrollbar, and adjust camera position an animation for different device widths.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,42 @@
 import "./App.css";
 
-import { useRef } from "react";
-import { Canvas } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+
+import { Canvas, useThree } from "@react-three/fiber";
 import { ScrollControls } from "@react-three/drei";
 
 import Experience from "./Experience";
 
-function App() {
-  const { cameraRef } = useRef();
+import { useControls } from "leva";
 
+function App() {
+  const [defaultCameraFOV, setDefaultCameraFOv] = useState(15);
+  const [defaultCameraPos, setDefaultCameraPos] = useState({
+    x: 5,
+    y: 2,
+    z: 15,
+  });
   return (
-    // For the camera to be "animatable," it needs to
-    // have an initial position defined, and ScrollControls
-    // must wrap your Experience component.
-    <Canvas
-      camera={{
-        fov: 15,
-        position: [5, 2, 15],
-      }}
-    >
-      {/* pages: Defines the length of the scroll area, each page is height:100% */}
-      <ScrollControls pages={1} damping={0.1}>
-        <Experience />
-      </ScrollControls>
-    </Canvas>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      {/* For the camera to be "animatable," it needs to
+      have an initial position defined, and 
+      ScrollControls must wrap your Experience component. */}
+      <Canvas
+        camera={{
+          fov: defaultCameraFOV,
+          position: [
+            defaultCameraPos.x,
+            defaultCameraPos.y,
+            defaultCameraPos.z,
+          ],
+        }}
+      >
+        {/* pages: Defines the length of the scroll area, each page is height:100% */}
+        <ScrollControls pages={1} damping={0.1}>
+          <Experience />
+        </ScrollControls>
+      </Canvas>
+    </div>
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,49 +1,120 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useScroll } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 
 import { Skyscraper } from "./Skyscraper";
-import { viewport } from "three/tsl";
 
 function Experience() {
+  const desktopFOV = 15;
+  const tabletFOV = 19;
+  const mobileFOV = 30;
+
+  // prettier-ignore
+  const tabletWidth = { max: 768, min: 426 };
+  const mobileWidth = { max: 425, min: 320 };
+
+  const [position, setPostion] = useState({
+    one: { x: 0, y: 0, z: 0 },
+    two: { x: 0, y: 0, z: 0 },
+    three: { x: 0, y: 0, z: 0 },
+  });
+
+  // prettier-ignore
+  const desktopPositions = {
+    One: {   x: -50, y: 10, z: 15},
+    Two: {   x: 30,  y: 15, z: 15},
+    Three: { x: 30,  y: 20, z: -20}
+  };
+  const tabletPositions = {
+    One: { x: -50, y: 10, z: 18 },
+    Two: { x: 30, y: 15, z: 18 },
+    Three: { x: 35, y: 20, z: -15 },
+  };
+  const mobilePositions = {
+    One: { x: -28, y: 10, z: 2 },
+    Two: { x: 25, y: 15, z: 5 },
+    Three: { x: 25, y: 22, z: -4 },
+  };
+
   // This grabs the <Canvas> camera
   const { camera } = useThree();
   const scroll = useScroll();
   const timeline = useRef();
 
+  // prettier - ignore
+  useEffect(() => {
+    if (window.screen.width > tabletWidth.max) {
+      // console.log(`Desktop = ${window.screen.width} > ${tabletWidth.max}`);
+      camera.fov = desktopFOV;
+      camera.updateProjectionMatrix();
+      // prettier-ignore
+      setPostion({
+        one: {   x: desktopPositions.One.x,   y: desktopPositions.One.y,   z: desktopPositions.One.z},
+        two: {   x: desktopPositions.Two.x,   y: desktopPositions.Two.y,   z: desktopPositions.Two.z},
+        three: { x: desktopPositions.Three.x, y: desktopPositions.Three.y, z: desktopPositions.Three.z},
+      });
+    } else {
+      if (window.screen.width >= tabletWidth.min) {
+        // console.log(`Tablet = ${window.screen.width} >= ${tabletWidth.min}`);
+        camera.fov = tabletFOV;
+        camera.updateProjectionMatrix();
+        // prettier-ignore
+        setPostion({
+            one: { x: tabletPositions.One.x, y: tabletPositions.One.y, z: tabletPositions.One.z },
+            two: { x: tabletPositions.Two.x, y: tabletPositions.Two.y, z: tabletPositions.Two.z },
+            three: { x: tabletPositions.Three.x, y: tabletPositions.Three.y, z: tabletPositions.Three.z }
+          });
+      }
+      if (mobileWidth.max >= window.screen.width) {
+        // console.log(`Mobile = ${mobileWidth.max} >= ${window.screen.width}`);
+        camera.fov = mobileFOV;
+        camera.updateProjectionMatrix();
+        // prettier-ignore
+        setPostion({
+            one: { x: mobilePositions.One.x, y: mobilePositions.One.y, z: mobilePositions.One.z },
+            two: { x: mobilePositions.Two.x, y: mobilePositions.Two.y, z: mobilePositions.Two.z },
+            three: { x: mobilePositions.Three.x, y: mobilePositions.Three.y, z: mobilePositions.Three.z }
+          });
+      }
+    }
+  }, [window.screen.width]);
+
   // 1. Create a "Master Timeline" that is PAUSED
-  useGSAP(() => {
-    timeline.current = gsap.timeline({ paused: true });
+  useGSAP(
+    () => {
+      timeline.current = gsap.timeline({ paused: true });
 
-    // STREET VIEW to FISH TANK
-    // Pretty good animation to get from first section to second section
-    // x: -50, y: 10, z: 15
-    timeline.current.to(camera.position, {
-      x: -50,
-      y: 10,
-      z: 15,
-      duration: 3, // Timeline duration is now 0 to 1
-    });
+      if (!position) return;
 
-    // FISH TANK to PROJECT 1
-    timeline.current.to(camera.position, {
-      x: 30,
-      y: 15,
-      z: 15,
-      duration: 3, // Timeline duration is now 0 to 1
-    });
+      // STREET VIEW to FISH TANK
+      timeline.current.to(camera.position, {
+        x: position.one.x,
+        y: position.one.y,
+        z: position.one.z,
+        duration: 3,
+      });
 
-    // PROJECT 1 to PROJECT 2
-    timeline.current.to(camera.position, {
-      x: 30,
-      y: 20,
-      z: -20,
-      duration: 3, // Timeline duration is now 0 to 1
-    });
-  }, [camera]);
+      // FISH TANK to PROJECT 1
+      timeline.current.to(camera.position, {
+        x: position.two.x,
+        y: position.two.y,
+        z: position.two.z,
+        duration: 3,
+      });
+
+      // PROJECT 1 to PROJECT 2
+      timeline.current.to(camera.position, {
+        x: position.three.x,
+        y: position.three.y,
+        z: position.three.z,
+        duration: 3,
+      });
+    },
+    { camera, dependencies: [position] },
+  );
 
   // 2. Use useFrame to sync the scroll progress to the timeline
   useFrame(() => {
@@ -57,7 +128,8 @@ function Experience() {
 
   return (
     <>
-      <color attach="background" args={["grey"]} />
+      <color args={["#000"]} attach="background" />
+      <fog attach="fog" args={["#413d3d", 2, 85]} />
       <ambientLight intensity={1.5} />
 
       {/* PROJECT 2 SECTION */}
